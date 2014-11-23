0bcc0685786a5fbcc37c748d646ce5022c8c40a0
Michael Mullins <michael@webdesserts.com>
2014-11-23 17:28:33 -0600

    remove lchab until it's ready

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ var rgb = require('alchemist-rgb')
 var xyz = require('alchemist-xyz')
 var hsl = require('alchemist-hsl')
 var lab = require('alchemist-lab')
-var lchab = require('alchemist-lchab')
 
-module.exports = [ xyz, rgb, hsl, lab, lchab ]
+module.exports = [ xyz, rgb, hsl, lab ]
